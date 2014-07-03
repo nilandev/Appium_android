9dcf519124fccc7970605979e2f1a6e4507df231
Nilanchala <nilanchalapanigrahy@gmail.com>
2014-07-03 15:15:17 +0100

    added new test class
    Signed-off-by: Nilanchala <nilanchalapanigrahy@gmail.com>

---
+++ LoginTest/src/com/imano/AllTestCases.java
@@ -0,0 +1,11 @@
+package com.imano;
+
+import junit.framework.Test;
+import junit.framework.TestSuite;
+import android.test.suitebuilder.TestSuiteBuilder;
+
+public class AllTestCases extends TestSuite {
+    public static Test suite() {
+        return new TestSuiteBuilder(AllTestCases.class).includeAllPackagesUnderHere().build();
+    }
+}
---
+++ LoginTest/src/com/imano/UserValidationTestcases.java
@@ -0,0 +1,18 @@
+package com.imano;
+
+import junit.framework.Assert;
+import android.test.AndroidTestCase;
+
+public class UserValidationTestcases extends AndroidTestCase {
+
+	private String username = "admin001";
+	private String password = "admin0005";
+
+	public void testUsernameLength() throws Throwable {
+		Assert.assertTrue(username.length() > 5 && username.length() < 20);
+	}
+
+	public void testPasswordLength() throws Throwable {
+		Assert.assertTrue(password.length() > 5 && password.length() < 20);
+	}
+}

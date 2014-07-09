015a06c691dff2cd3215011cf67de374301eed4f
Nilanchala <nilanchalapanigrahy@gmail.com>
2014-07-09 18:16:17 +0100

    changed env
    Signed-off-by: Nilanchala <nilanchalapanigrahy@gmail.com>

--- features/support/env.rb
+++ features/support/env.rb
@@ -15,8 +15,8 @@ end
 # Load the desired configuration from appium.txt, create a driver then
 # Add the methods to the world
 
-# caps = Appium.load_appium_txt file: File.expand_path('./', __FILE__), verbose: true
-# Appium::Driver.new(caps)
+caps = Appium.load_appium_txt file: File.expand_path('./', __FILE__), verbose: true
+Appium::Driver.new(caps)
 Appium.promote_appium_methods AppiumWorld
 
 World do
